5b53f1bca690d830efd23b3ffff223ab91384744
pezzettii <pezzettii@hotmail.com>
2025-05-09 17:12:22 -0300

    Delete desafio 6.py

--- desafio 6.py
+++
@@ -1,5 +0,0 @@
-num=int(input('Digite um numero: '))
-dobro=num*2
-triplo=num*3
-raiz=num**(1/2)
-print('O dobro do numero digitado e {}; \nO triplo do numero digitado e {}; \nA raiz quadrada do numero  digitado e {}' .format(dobro,triplo,raiz))
