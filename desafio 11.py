3baffecfa901f71ae6634ef6053953d6fcc2cb02
pezzettii <pezzettii@hotmail.com>
2025-05-09 17:11:37 -0300

    Delete desafio 11.py

--- desafio 11.py
+++
@@ -1,5 +0,0 @@
-alt=float(input("Digite a altura em metros: "))
-larg=float(input("Digite a largura em metros: "))
-m2=alt*larg
-tinta=m2/2
-print('É necessário {:.2f} litros de tinta.'.format(tinta))
